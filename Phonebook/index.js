a64cf79156ca699b0394b9103f5b66616641f975
SideMikkoSalhoja <mikko.salhoja.side@gmail.com>
2024-12-06 18:16:49 +0200

    web vision

--- Phonebook/index.js
+++ Phonebook/index.js
@@ -31,6 +31,7 @@ morgan.token('newWay', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
   });
 
+app.use(express.static('dist'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms - :newWay'))
 app.use(express.json())
